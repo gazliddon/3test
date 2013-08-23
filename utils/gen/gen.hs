d748ff3f9f5b2244f64ce771387912b4629c2479
gazbuntu <gaz@gazaxian.com>
2013-08-23 13:56:06 +0100

    adding command line stuff

--- utils/gen/gen.hs
+++ utils/gen/gen.hs
@@ -4,6 +4,7 @@ import Data.Function
 
 -- 3d Points - 9 lines for a simple vector class
 data Point = Pt {x,y,z :: Float} deriving (Show)
+fromArray [xx,yy,zz] = Pt xx yy zz
 sub (Pt x y z) (Pt x1 y1 z1) = Pt (x-x1) (y-y1) (z-z1)
 add (Pt x y z) (Pt x1 y1 z1) = Pt (x+x1) (y+y1) (z+z1)
 dot (Pt x y z) (Pt x1 y1 z1) = x*x1 + y*y1 + z*z1 
@@ -41,42 +42,39 @@ convHull xs =
     (firstPoint:pointsSortedByX) = sortBy (compare `on` x) xs
 
 sortFunc myUp base a b = compare (angleFromVec myUp base a) (angleFromVec myUp base b)
-
 sortByAngle myUp base = sortBy (sortFunc myUp base)
 
 --- Support nonsense for generating points and printing things out
 into3 (a:b:c:xs) = (a,b,c) : into3 xs
-
 randomPoints seed = map (\(x,y,z) -> Pt x y 0) $ into3 $ randomRs (0,1) (mkStdGen seed)
 
-generator:: (Num a) => (a -> a) -> [a -> a] -> Integer -> [[a]]
 generator fn funs n
   | n == 0 = []
-  | otherwise = (map ($ v) funs) : generator fn funs (n-1)
+  | otherwise = (map ($ v) funs) : generator fn funs (n - 1)
   where
-    floatingN = fromIntegral n
-    v = fn (fromIntegral ( 1.0 / floatingN))
+    v = fn  $ 1.0 / (fromIntegral n)
 
-circPoints3 n = generator (* (2 * pi)) [(cos),(sin),(* 0.0)] n
-
-
-cp = map (\[x, y, z] -> (Pt x y z)) (circPoints3 10)
-
-
-circPoints n 
-  | n == 0 = []
-  | otherwise = Pt (cos pos) (sin pos) 0 : circPoints (n - 1)
-  where
-   floatn = fromIntegral n
-   pos = (2 * pi / floatn) * floatn
+circPoints n = map fromArray $ generator (* (2 * pi)) [cos,sin,(*0)] n
+cp = circPoints 10
 
 makeJson (Pt x y z) = "[ " ++ show x ++ ", " ++ show y ++ ", " ++ show z ++ "]" 
 
-printHull name chull = do
-    putStr $ name ++ " = [\n  "
-    putStrLn $ intercalate ",\n  " (map makeJson chull)
-    putStr "]\n"
+hullStr chull = do
+    "[" ++ (intercalate "," (map makeJson chull)) ++ "]"
 
 -- Test
-main = printHull "obj" $ convHull (take 400 (randomPoints 123))
-  
+name = "objs"
+numOfObjs = 30
+seed = 123
+complexity = 100
+
+objList seed =  convHull (take complexity (randomPoints seed)) : objList (seed +1)
+
+main =
+  let
+    wrapBrace x = "[" ++ x ++ "]"
+    hullToStr h = wrapBrace $ concatMap makeJson h
+    hullStr = name ++ "=" ++ (wrapBrace $ concatMap hullToStr (take numOfObjs (objList seed)))
+  in do
+    putStrLn hullStr
+
